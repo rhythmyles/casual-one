0699d6706803812fcaa10f36532064b511ad5242
zhanglei <rhythmyles@163.com>
2021-12-17 11:54:14 +0800

    casual-one initial

---
+++ src/main/java/ltd/lant/casualone/CasualOneApplication.java
@@ -0,0 +1,13 @@
+package ltd.lant.casualone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasualOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasualOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ltd/lant/casualone/CasualOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.lant.casualone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasualOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
