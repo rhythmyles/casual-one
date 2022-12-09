644373fd2377c27a4af1057cc6a8364d7b40cd8d
myles <349916188@qq.com>
2022-12-09 16:34:20 +0800

    project init...

--- casualone-web/src/test/java/ltd/lant/casualone/test/CasualOneApplicationTests.java
+++ casualone-web/src/test/java/ltd/lant/casualone/test/CasualOneApplicationTests.java
@@ -1,10 +1,13 @@
 package ltd.lant.casualone.test;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Optional;
@@ -36,14 +39,14 @@ class CasualOneApplicationTests {
             System.err.println("readLock: " + readLock);
             boolean validate = lock.validate(readLock);
             System.out.println("validate: " + validate);
-            if (validate){
+            if (validate) {
                 readLock = lock.readLock();
                 try {
                     System.out.println("read age: " + user.getAge());
                 } finally {
                     lock.unlockRead(readLock);
                 }
-            }else {
+            } else {
                 System.out.println("read age: " + user.getAge());
             }
         };
@@ -57,16 +60,22 @@ class CasualOneApplicationTests {
     }
 
     @Test
-    void optionalTest(){
+    void optionalTest() {
         User zhangsan = new User("zhangsan", 18);
         User lisi = null;
         User wangwu = new User("wangwu", 81);
         User user = Optional.ofNullable(zhangsan).orElse(wangwu);
         User user1 = Optional.ofNullable(lisi).orElse(wangwu);
+
+        Optional<User> optUser1 = Optional.of(user);
+        User user2 = optUser1.get();
+
+        Optional.ofNullable(user1).ifPresent(p -> System.out.println("年龄"+p.getAge()));
+
     }
 
     @Test
-    void assertTest(){
+    void assertTest() {
         try {
             User user = null;
             //     user = new User("zhaoliu", 80);
@@ -76,6 +85,7 @@ class CasualOneApplicationTests {
             System.err.println("Exception | Error");
         }
     }
+
     @Test
     void futureTest() {
         ExecutorService threadPool = Executors.newFixedThreadPool(5);
@@ -83,7 +93,7 @@ class CasualOneApplicationTests {
             String name = "";
             for (int i = 0; i < 1000; i++) {
                 name = Thread.currentThread().getName();
-            //    System.out.println(name + "----" + i);
+                //    System.out.println(name + "----" + i);
             }
             System.out.println(name + "执行完成！");
             return null;
@@ -92,7 +102,7 @@ class CasualOneApplicationTests {
             String name = "";
             for (int i = 0; i < 100000; i++) {
                 name = Thread.currentThread().getName();
-                if (i % 10000 == 0){
+                if (i % 10000 == 0) {
                     System.err.println(name + "----" + i);
                 }
             }
@@ -107,11 +117,26 @@ class CasualOneApplicationTests {
     }
 
     @Test
-    void dateTest(){
+    void paint() {
+        String s = "]7D8^<;>]367381:\\2;:5::4N2>2::9<B7K9835;3@K893;>V6;2n6<2k6?3i3C3I6J3C3I7I3C3J5J3C3i3C3i372:3j22893j218:3j326:3[4;:;3I3:9;7>3I4:8;3B3I4:7<3B3J63:=3B3K=B3B3N7E3B3j3?6j3;:j3:443j39263j3:173j3:173j3:272j3;362i3<;h3?8g3F4d3H5`4K7V8Q=<EYRh696m696m696m696m696m878k969k787L", t = "";
+        char f = 48;
+        int n = 1, i, j;
+        for (i = 0; i < 265; i++) {
+            for (j = 0; j < s.charAt(i) - '0'; j++) {
+                t += f;
+                if (n++ % 82 == 0) t += '\n';
+            }
+            f = (f == 48 ? ' ' : '0');
+        }
+        System.out.print(t);
+    }
+
+    @Test
+    void dateTest() {
 
         Date date1 = new Date(0L); // 0时区的0点,东8区的8点
-        Date date2 = new Date(3600*1000L); //0时区的1点,东8区的9点
-        Date date3 = new Date(7200L*1000); //0时区的3点,东8区的11点
+        Date date2 = new Date(3600 * 1000L); //0时区的1点,东8区的9点
+        Date date3 = new Date(7200L * 1000); //0时区的3点,东8区的11点
 
         Date date4 = new Date(date1.getTime() + date2.getTime());
         Date date5 = new Date(date1.getTime() + date2.getTime() + date3.getTime());
@@ -119,35 +144,44 @@ class CasualOneApplicationTests {
         System.out.println(date5);
     }
 
-    class User {
+    @Test
+    void integerTest() throws NoSuchFieldException, IllegalAccessException {
+        Class cache = Integer.class.getDeclaredClasses()[0];
+        Field myCache = cache.getDeclaredField("cache");
+        myCache.setAccessible(true);
 
-        public User(int age){
+        Integer[] newCache = (Integer[]) myCache.get(cache);
+        newCache[132] = newCache[138];
+        int a = 2;
+        int b = a + a;
+        System.out.printf("%d + %d = %d", a, a, b);
+    }
+
+
+
+    class User {
+        public User(int age) {
             this.age = age;
         }
-
-        public User(String name, int age){
+        public User(String name, int age) {
             this.name = name;
             this.age = age;
         }
-
         private String name;
         private int age;
-
-
         public int getAge() {
             return age;
         }
-
         public void setAge(int age) {
             this.age = age;
         }
-
         public String getName() {
             return name;
         }
-
         public void setName(String name) {
             this.name = name;
         }
     }
+
+
 }
